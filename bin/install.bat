4c8b5073f20d7363b8b31c81a0404245ec280729
Marcus Ottosson <konstruktion@gmail.com>
2019-06-11 12:26:42 +0100

    Improved feedback in cmd

--- bin/install.bat
+++ bin/install.bat
@@ -3,7 +3,7 @@ setlocal
 
 :: Leverage Rez's Python to avoid Python as a dependency
 pushd %~dp0..\python
-call rez python -m "" scoopz %*
+call rez python -u "" -m scoopz %*
 
 :: Regarding pushd
 :: rez-python does not consider PYTHONPATH, as it is
@@ -11,8 +11,8 @@ call rez python -m "" scoopz %*
 :: leverage the fact that it considers whatever is on
 :: its current working directory as a valid package.
 
-:: Regarding the -m ""
-:: rez-python considers the first unknown argument as an
+:: Regarding the -u ""
+:: rez-python considers the second unknown argument as an
 :: argument to FILE, which is incorrect. In effect, ""
 :: is accepted by rez-python, and internally discarded
 :: as it is empty, which makes it ignored
